0ffe2a412da3cd9150c0837bfaca0a2839df4e62
Tanmay Wankar <62742498+tanmaywankar@users.noreply.github.com>
2026-03-04 18:19:09 +0530

    added all the basic shape masks

--- app.js
+++ app.js
@@ -1,37 +1,90 @@
 const state = {
-  eyeOffset: 70,
   eyeRadius: 45,
-  canvasX: 0, 
+  eyeGap: 20,
+  eY: 60,
+  canvasX: 0,
   canvasY: 0,
-  canvasActualX:0,
-  canvasActualY:120,
 };
 
+const mood = {
+  topH: 10,
+  botH: 1,
+  tilt: 60,
+  pR: 30,
+  eR: 45,
+};
+
+let eyeOffset = mood.eR + state.eyeGap;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = "#111";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle = "#000";
-ctx.fillRect(0 + state.canvasX, (state.canvasActualY/2)+ state.canvasY, 320, 120);
+function renderEmotions() {
+  //for filling black color every new frame
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  ctx.fillStyle = "#111";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //for getting sprite canvas i.e the 320 * 120 grid
+  let spriteTop = state.eY + state.canvasY;
+  ctx.translate(state.canvasX, spriteTop);
+
+  //actual workable eye canvas
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, 320, 120);
+
+  drawEye(state.eY, mood.eR, mood.pR, mood.topH, mood.botH, mood.tilt, false);
+  drawEye(state.eY, mood.eR, mood.pR, mood.topH, mood.botH, mood.tilt, true);
+}
+renderEmotions();
+
+function drawEye(eY, eR, pR, lidH, botH, tilt, isLeft) {
+  // eyes
+  let eyePosition;
+  if (!isLeft) {
+    eyePosition = 160 + eyeOffset;
+  } else {
+    eyePosition = 160 - eyeOffset;
+  }
+  ctx.beginPath();
+  ctx.arc(eyePosition, eY, eR, 0, Math.PI * 2);
+  ctx.fillStyle = "#94ff00";
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(eyePosition, eY, pR, 0, Math.PI * 2);
+  ctx.fillStyle = "#000000";
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.fillStyle = "#080707";
+  ctx.fillRect(eyePosition - eR, (eY - eR) - 3, 2 * eR, lidH);
+
+  ctx.beginPath();
+  ctx.fillStyle = "#080707";
+  ctx.fillRect(eyePosition - eR, (eY + eR) + 2, 2 * eR, botH);
+
+  let pointX;
+if (isLeft) {
+    pointX = (tilt < 0) ? (eyePosition - eR) : (eyePosition + eR);
+} else {
+    pointX = (tilt < 0) ? (eyePosition + eR) : (eyePosition - eR);
+}
 
 ctx.beginPath();
-ctx.arc(80, state.canvasActualY, 40, 0, Math.PI * 2);
-ctx.fillStyle = "#94ff00";
-ctx.fill();
+ctx.fillStyle = "#080707";
 
-ctx.beginPath();
-ctx.arc(80, state.canvasActualY, 25, 0, Math.PI * 2);
-ctx.fillStyle = "#000000";
+if(tilt !== 0){
+ctx.moveTo(eyePosition - eR, (eY - eR + lidH) - 3); 
+ctx.lineTo(eyePosition + eR, (eY - eR + lidH) - 3); 
+ctx.lineTo(pointX, eY - eR + lidH + Math.abs(tilt)); 
+ctx.closePath();
 ctx.fill();
+}
 
+}
 
-ctx.beginPath();
-ctx.arc(240, state.canvasActualY, 40, 0, Math.PI * 2);
-ctx.fillStyle = "#94ff00";
-ctx.fill();
+function lookAt(x, y) {
+  state.canvasX = x;
+  state.canvasY = y;
 
-ctx.beginPath();
-ctx.arc(240, state.canvasActualY, 25, 0, Math.PI * 2);
-ctx.fillStyle = "#000000";
-ctx.fill();
+  renderEmotions();
+}
